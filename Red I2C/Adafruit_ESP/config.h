2f8acf845026fbf0a4b74ca24773b460c0d75b3a
Joaquín Enrique Fuentes Rodríguez <joaquin.enriquefr@gmail.com>
2026-08-14 00:51:28 -0600

    Update config.h no io key

--- Red I2C/Adafruit_ESP/config.h
+++ Red I2C/Adafruit_ESP/config.h
@@ -2,8 +2,8 @@
 
 // visit io.adafruit.com if you need to create an account,
 // or if you need your Adafruit IO key.
-#define IO_USERNAME  "LF_AC"
-#define IO_KEY       "aio_SLfl31SXXEPe9LBEhfCFqryRUMWx"
+#define IO_USERNAME  ""
+#define IO_KEY       ""
 
 /******************************* WIFI **************************************/
 
